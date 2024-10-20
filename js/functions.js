3fb9c3a7e9b98172c80b20b9a59d6dd98a99fb4b
Ваня Городничев <lennygames172@gmail.com>
2024-10-20 15:24:14 +0500

    Функции для тренировки1

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,23 @@
 function checkTheString(string, maxLength) {
   if (string.length <= maxLength) {
-    return string.length <= maxLength;
+    return string.length <= maxLength
   }
 }
 // Cтрока короче 20 символов
-console.log(checkTheString('проверяемая строка', 20)); // true
+checkTheString('проверяемая строка', 20); // true
 // Длина строки ровно 18 символов
-console.log(checkTheString('проверяемая строка', 18)); // true
+checkTheString('проверяемая строка', 18); // true
 // Строка длиннее 10 символов
-console.log(checkTheString('проверяемая строка', 10)); // false
+checkTheString('проверяемая строка', 10); // false
 
 function PalindrimeString(string) {
   const newString = string.toLowerCase();
-  return newString === newString.split('').reverse().join('')
+  return newString === newString.split('').reverse().join('');
 }
 // Строка является палиндромом
-console.log(PalindrimeString('топот')); // true
+PalindrimeString('топот'); // true
 // Несмотря на разный регистр, тоже палиндром
-console.log(PalindrimeString('ДовОд')); // true
+PalindrimeString('ДовОд'); // true
 // Это не палиндром
-console.log(PalindrimeString('Кекс'));  // false
+PalindrimeString('Кекс'); // false
 
