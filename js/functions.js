7eed7fcde3151e57304e0961de5c6afb9eb7a718
Ваня Городничев <lennygames172@gmail.com>
2024-10-26 16:30:14 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,26 @@ PalindrimeString('ДовОд'); // true
 // Это не палиндром
 PalindrimeString('Кекс'); // false
 
+function parseTime(timeString) {
+  const [hours, minutes] = timeString.split(':').map(Number);
+  return hours * 60 + minutes;
+}
+
+function checkMeetingTime(startTime, endTime, meetingStart, meetingDuration) {
+  const startMinutes = parseTime(startTime);
+  const endMinutes = parseTime(endTime);
+  const meetingStartMinutes = parseTime(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  if (meetingEndMinutes > endMinutes || meetingStartMinutes < startMinutes) {
+    return false;
+  }
+
+  return meetingEndMinutes <= endMinutes && meetingStartMinutes >= startMinutes;
+}
+
+console.log(checkMeetingTime('08:00', '17:30', '14:00', 90)); // true
+console.log(checkMeetingTime('8:0', '10:0', '8:0', 120));     // true
+console.log(checkMeetingTime('08:00', '14:30', '14:00', 90));  // false
+console.log(checkMeetingTime('14:00', '17:30', '08:0', 90));   // false
+console.log(checkMeetingTime('8:00', '17:30', '08:00', 900)); // false
